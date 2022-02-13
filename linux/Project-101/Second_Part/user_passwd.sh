0ff0555d2f726b4adba9d97abfb193bcb81447c8
Mehmet Fatih Yar <90246820+Fatalrhym@users.noreply.github.com>
2022-02-13 23:28:24 +0300

    13.02.22

--- linux/Project-101/Second_Part/user_passwd.sh
+++ linux/Project-101/Second_Part/user_passwd.sh
@@ -7,37 +7,40 @@
 # Make sure the script is being executed with superuser privileges.
 
 # Get the username (login).
-read -p "Please login your username : " username
+username=$1
 
 # Get the real name (contents for the description field).
 read -p "Please enter your name : " name
 
 # Get the password.
 
-read -sp "Please enter your password : " password
+password () { < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-16}; echo;}
+
 
 # Create the account.
 
-sudo useradd -c "$name" $username
+sudo useradd -c "$name" $1
 
 # Check to see if the useradd command succeeded.
 echo -e "\n"
 
-cat /etc/passwd | grep "$username"
+cat /etc/passwd | grep "$1"
 
 # We don't want to tell the user that an account was created when it hasn't been.
 
 # Set the password.
 
-sudo passwd -p $username 
-sudo passwd $password
-
 # Check to see if the passwd command succeeded.
-sudo cat /etc/shadow 
+
+sudo cat /etc/shadow | grep $1
+
 # Force password change on first login.
 
 # Display the username, password, and the host where the user was created.
 
 echo "Your username is $username "
-echo "Your password is $password "
+echo "Your name is $name "
+echo "Your password is "
+password
+
 
