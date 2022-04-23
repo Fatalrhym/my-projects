15587660a18cfa39841a951f8905f298441ce5a1
Mehmet Fatih Yar <90246820+Fatalrhym@users.noreply.github.com>
2022-04-23 17:59:33 +0300

    23.04.22

--- devops/203-dockerization-bookstore-api-on-python-flask-mysql/main.tf
+++ devops/203-dockerization-bookstore-api-on-python-flask-mysql/main.tf
@@ -18,7 +18,7 @@ provider "aws" {
 
 provider "github" {
   # Configuration options
-  token = "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"
+  token = "xxxxxxxxxxxxxxxxxx"
 }
 
 resource "github_repository" "myrepo" {
@@ -63,7 +63,7 @@ resource "aws_instance" "tf-docker-ec2" {
           -o /usr/local/bin/docker-compose
           chmod +x /usr/local/bin/docker-compose
           mkdir -p /home/ec2-user/bookstore-api
-          TOKEN="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"
+          TOKEN="xxxxxxxxxxxx"
           FOLDER="https://$TOKEN@raw.githubusercontent.com/Fatalrhym/bookstore-repo/main/"
           curl -s --create-dirs -o "/home/ec2-user/bookstore-api/app.py" -L "$FOLDER"bookstore-api.py
           curl -s --create-dirs -o "/home/ec2-user/bookstore-api/requirements.txt" -L "$FOLDER"requirements.txt
